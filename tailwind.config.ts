26b46080629cfb37b90988096de980160257baa4
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-10-25 19:02:52 -0300

    feat: add Space Mono font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        spaceMono: ['--space-mono'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
