057bc61489bcfd738d5da6713b2adc6f6d093c72
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-05-17 10:20:59 -0300

    style: on-screen style adjustments not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
-'use client'
-
+import { Metadata } from 'next'
 import Link from 'next/link'
 import { MoveLeft } from 'lucide-react'
 
+export const metadata: Metadata = {
+  title: '404',
+}
+
 export default function NotFoundPage() {
   return (
-    <main className="mx-auto flex max-w-[41.25rem] flex-col gap-y-8 pt-24">
+    <main className="mx-auto flex max-w-[41.25rem] flex-col gap-y-8 px-4 pt-24">
       <h1 className="text-primary text-5xl font-semibold leading-none">404</h1>
       <div className="flex flex-col gap-y-2">
         <h2>Page is not found</h2>
@@ -14,7 +17,7 @@ export default function NotFoundPage() {
         </p>
       </div>
       <div>
-        <Link href="/" className="flex flex-row items-center gap-2">
+        <Link href="/" className="flex w-fit flex-row items-center gap-2">
           <MoveLeft size={12} strokeWidth={2} />
           Back to home
         </Link>
